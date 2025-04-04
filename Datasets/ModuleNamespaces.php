120fc5d7d7198826f4a4ccf95b38aa73cf69861b
Joe Cianflone <JoeCianflone@users.noreply.github.com>
2025-04-04 13:55:54 -0400

    feature: removes Feature and Dataset from submodule

--- Datasets/ModuleNamespaces.php
+++
@@ -1,7 +0,0 @@
-<?php declare(strict_types=1);
-
-dataset('modulenamespaces', function () {
-    return collect(glob('modules/*'))
-      ->map(fn (string $dir, int $key): string => ucfirst(str_replace('modules/', '', $dir)))
-      ->toArray();
-});
